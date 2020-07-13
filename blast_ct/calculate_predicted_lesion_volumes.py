03d46817b691e917a964aaf6072046a9e4711c7e
MiguelMonteiro <mab.mtr@gmail.com>
2020-07-13 16:35:35 +0100

    added script to computed predicted lesion volumes

---
+++ blast_ct/calculate_predicted_lesion_volumes.py
@@ -0,0 +1,25 @@
+import argparse
+import pandas as pd
+import SimpleITK as sitk
+import numpy as np
+
+CLASS_NAMES = ['background', 'iph', 'eah', 'oedema', 'ivh']
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--prediction-csv-path',
+                        required=True,
+                        type=str,
+                        help='Path to the prediction csv file.')
+    parse_args, unknown = parser.parse_known_args()
+    prediction_csv_path = parse_args.prediction_csv_path
+    dataframe = pd.read_csv(prediction_csv_path, index_col='id')
+    for id, row in dataframe.iterrows():
+        prediction = sitk.ReadImage(row['prediction'])
+        voxel_volume_ml = np.prod(prediction.GetSpacing()) / 1000.
+        array = sitk.GetArrayFromImage(prediction)
+        for i, class_name in enumerate(CLASS_NAMES):
+            if i == 0:
+                continue
+            dataframe.loc['id', f'{class_name:s}_predicted_volume_ml'] = np.sum(prediction == i) * voxel_volume_ml
+    dataframe.to_csv(prediction_csv_path)
