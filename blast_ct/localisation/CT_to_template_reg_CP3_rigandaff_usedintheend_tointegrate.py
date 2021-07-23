b5873a842b39266556aa5422ce85273654aee54f
cpicarra <87124955+cpicarra@users.noreply.github.com>
2021-07-23 09:49:25 +0100

    Update CT_to_template_reg_CP3_rigandaff_usedintheend_tointegrate.py

--- blast_ct/localisation/CT_to_template_reg_CP3_rigandaff_usedintheend_tointegrate.py
+++ blast_ct/localisation/CT_to_template_reg_CP3_rigandaff_usedintheend_tointegrate.py
@@ -106,7 +106,7 @@ class RegistrationToCTTemplate(object):
 
     def best_run(self, final_metric_aff_dict):
         sm_values = {}
-        for iteration in range(1, no_runs):
+        for iteration in range(1, self.no_runs):
             sm_values[iteration] = final_metric_aff_dict.get(iteration,{}).get('final_metric_aff')
         best_iter = min(sm_values.items(), key=operator.itemgetter(1))[0]
         transform, iterations_rig, final_metric_rig, iterations_aff, final_metric_aff, image_resampled_aff = final_metric_aff_dict[best_iter].values()
