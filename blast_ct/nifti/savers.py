a26e3e5c0d581df1fb6b3ae6c165405b902ee4ed
cpicarra <87124955+cpicarra@users.noreply.github.com>
2021-07-27 13:33:48 +0100

    Update savers.py

--- blast_ct/nifti/savers.py
+++ blast_ct/nifti/savers.py
@@ -129,7 +129,7 @@ class NiftiPatchSaver(object):
             for name, array in to_write.items():
                 path = os.path.join(self.prediction_dir, f'{str(id_):s}_{name:s}.nii.gz')
                 self.data_index.loc[self.data_index['id'] == id_, name] = path
-                save_image(array, input_image, path, self.localisation_dir, _id, self.data_index, self.localisation,
+                save_image(array, input_image, path, self.localisation_dir, id_, self.data_index, self.localisation,
                            self.write_registration_info, self.number_of_runs, self.native_space, resolution)
                 if name == 'prediction':
                     resolution_ = resolution if resolution is not None else input_image.GetSpacing()
