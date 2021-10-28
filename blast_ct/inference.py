b6b4d1a001cd04ae62312c045d0b24249e63daf1
cpicarra <87124955+cpicarra@users.noreply.github.com>
2021-10-28 09:39:25 +0100

    fixing arg parser

--- blast_ct/inference.py
+++ blast_ct/inference.py
@@ -9,6 +9,16 @@ from blast_ct.read_config import get_model, get_test_loader
 from blast_ct.train import set_device
 from blast_ct.trainer.inference import ModelInference, ModelInferenceEnsemble
 
+def str2bool(v):
+    if isinstance(v, bool):
+        return v
+    if v.lower() in ('yes', 'true', 't', 'y', '1'):
+        return True
+    elif v.lower() in ('no', 'false', 'f', 'n', '0'):
+        return False
+    else:
+        raise argparse.ArgumentTypeError('Boolean value expected.')
+
 def new_dataset(test_csv_path, previous_prediction_csv_path):
     test_csv = pd.read_csv(test_csv_path)
     previous_prediction_csv = pd.read_csv(previous_prediction_csv_path)
@@ -20,7 +30,6 @@ def new_dataset(test_csv_path, previous_prediction_csv_path):
     test_csv_path = '/tmp/dataframe_yet_to_run.csv'
     dataframe_yet_to_run.to_csv(test_csv_path, index = False)
     return test_csv_path
-
 
 def run_inference(job_dir, test_csv_path, config_file, device, saved_model_paths, write_prob_maps, localisation,
                   num_reg_runs, overwrite, native_space):
@@ -73,40 +82,36 @@ def inference():
                         help='Directory for checkpoints, exports, and '
                              'logs. Use an existing directory to load a '
                              'trained model, or a new directory to retrain')
+
     parser.add_argument('--test-csv-path',
                         default=None,
                         type=str,
                         help='Path to test csv file with paths of images and masks.')
+
     parser.add_argument('--config-file',
                         default=default_config,
                         type=str,
                         help='A json configuration file for the job (see example files)')
+
     parser.add_argument('--device',
                         type=str,
                         default='cpu',
                         help='Device to use for computation')
+
     parser.add_argument('--saved-model-paths',
                         default=default_model_paths,
                         type=str,
                         help='Path to saved model or list of paths separated by spaces.')
 
     parser.add_argument('--write-prob-maps',
-                        dest='write-prob-maps',
-                        action='store_true',
+                        type=str2bool, nargs='?',
+                        default=False,
                         help='Whether to write probability maps images to disk')
-    parser.add_argument('--not-write-prob-maps',
-                        dest='write-prob-maps',
-                        action='store_false')
-    parser.set_defaults(write_prob_maps=False)
 
     parser.add_argument('--do-localisation',
-                        dest='localisation',
-                        action='store_true',
-                        help='Whether to overwrite run if already exists')
-    parser.add_argument('--not-do-localisation',
-                        dest='localisation',
-                        action='store_false')
-    parser.set_defaults(localisation=False)
+                        type=str2bool, nargs='?',
+                        default=False,
+                        help='Whether to run localisation or not')
 
     parser.add_argument('--num-reg-runs',
                         default=1,
@@ -114,26 +119,14 @@ def inference():
                         help='How many times to run registration between native scan and CT template.')
 
     parser.add_argument('--overwrite',
-                        dest='overwrite',
-                        action='store_true',
+                        type=str2bool, nargs='?',
+                        default=False,
                         help='Whether to overwrite run if already exists')
-    parser.add_argument('--not-overwrite',
-                        dest='overwrite',
-                        action='store_false')
-    parser.set_defaults(overwrite=False)
 
     parser.add_argument('--native-space',
+                        type=str2bool, nargs='?',
                         default=True,
-                        type=bool,
                         help='Whether to calculate the volumes in native space or atlas space.')
-    parser.add_argument('--native-space',
-                        dest='native-space',
-                        action='store_true',
-                        help='Whether to calculate the volumes in native space or atlas space.')
-    parser.add_argument('--atlas-space',
-                        dest='native-space',
-                        action='store_false')
-    parser.set_defaults(native_space=False)
 
     parse_args, unknown = parser.parse_known_args()
 
